69444761d5a58b7ceca07e4234a49942c893b51b
Felipe Albuquerque <rockalbuquerque@gmail.com>
2022-08-01 19:31:37 -0300

    Bug corrigdo no form (não aceitava espaço em branco)

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,7 +15,18 @@ export default function Form({ selectedSeatsIds, selectedSeatsNumbers, movieName
         }
 
         for(let i = 0; i < name.length; i++) {
-            if(isNaN(name[i]) === false) {
+            if(
+                name[i] === "0" || 
+                name[i] === "1" || 
+                name[i] === "2" || 
+                name[i] === "3" || 
+                name[i] === "4" || 
+                name[i] === "5" || 
+                name[i] === "6" || 
+                name[i] === "7" || 
+                name[i] === "8" || 
+                name[i] === "9" 
+            ) {
                 setName("");
                 return alert("Nome inválido!");
             }
